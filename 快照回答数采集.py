a5d3b2e58d592f915f4ac7cce5a89d7ada0af2d4
ginson <kingsionwang@gmail.com>
2017-04-24 16:33:04 +0800

    增加异常处理

--- 快照回答数采集.py
+++ 快照回答数采集.py
@@ -9,22 +9,32 @@ from UA import makeHeaders
 def getCacheURL(keyword):
     r = requests.get('http://www.baidu.com/s?wd=%s' %keyword,proxies=chProxy(),headers=makeHeaders())
     s = BeautifulSoup(r.text,'html.parser')
-    return s.find('a',href=re.complie('http://cache.baiducontent.com.*'))
+    return s.find('a',href=re.compile('http://cache.baiducontent.com.*'))['href']
 
-def getAnsNum(cacheurl):
-    r = requests.get(url,proxies=chProxy(),headers=makeHeaders())
+def getAnsNum(cacheURL):
+    r = requests.get(cacheURL,proxies=chProxy(),headers=makeHeaders())
     s = BeautifulSoup(r.text,'html.parser')
-    return s.findAll('div','title title2')[0].span.getText()
+    try:
+        AnsNum = s.findAll('div','title title2')[0].span.getText()
+    except AttributeError:
+        return 0
+    else:
+        return AnsNum
 
 if __name__ == '__main__':
-    keywords = open(sys.argv[1],'r').readlines()
+    keywords = open(sys.argv[1],'r').readlines()#[:10]
     rst = open('result.txt','w')
 
     for i in keywords:
         i = i.strip()
-        url = getCacheURL(i)
-        ansNum=getAnsNum(url)
-        rst.write('%s\t%s\n' %(i,ansNum))
-        rst.flush()
+        try:
+            url = getCacheURL(i)
+            ansNum=getAnsNum(url)
+        except:
+            continue
+        else:
+            print('%s\t%s' %(i,ansNum))
+            #rst.write('%s\t%s\n' %(i,ansNum))
+            #rst.flush()
 
     rst.close()
