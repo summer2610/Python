db422e4de9fe01afa7ccbeb549494eea4ec2309a
ginson <kingsionwang@gmail.com>
2017-04-25 18:30:54 +0800

    update

--- so内容数判断.py
+++ so内容数判断.py
@@ -6,8 +6,10 @@
 '''
 
 
-import requests,threading,time,os,sys,json,random
+import requests,time,os,sys,json,random
+from multiprocessing import Process,Lock
 from bs4 import BeautifulSoup
+from proxypool import chProxy
 
 def chnls(keyword,soup):
     return len(soup.findAll('h3','Seotitle-h3 clear'))
@@ -43,55 +45,54 @@ def ques(keyword,soup):
     return n
 
 def run(keyword):
-    r = requests.get('http://so.to8to.com/so.php?keyword=%s' %keyword,proxies=chooseproxy())
-    soup = BeautifulSoup(r.text,'lxml')
     try:
+        proxies = chProxy()
+        print('%s\t获取代理IP -- 完成' %os.getpid())
+        r = requests.get('http://so.to8to.com/so.php?keyword=%s' %keyword,proxies=proxies,timeout=30)
+        print('%s\t请求so页面 -- 完成' %os.getpid())
+        soup = BeautifulSoup(r.text,'lxml')
+        print('%s\t获取源码 -- 完成' %os.getpid())
         channel = chnls(keyword,soup)
         picture = pics(keyword,soup)
         article = arts(keyword,soup)
         question = ques(keyword,soup)
+        print('%s\t提取频道数/内容数 -- 完成' %os.getpid())
     except:
-        result.write('%s 出错\n'%keyword)
+        lock.acquire()
+        print('%s 出错\n'%keyword)
+        text.append(keyword)
+        print('%s\t出错关键词放进循环尾部' %os.getpid())
+        lock.release()
     else:
         lock.acquire()
+        print('%s,%s,%s\n' %(keyword,channel,picture+article+question))
         result.write('%s\t%s\t%s\n' %(keyword,channel,picture + article + question))
         lock.release()
-
-#获取代理
-def chooseproxy():
-    proxypool = json.loads(requests.get('http://127.0.0.1:8000/?count=50').text)
-    proxy = random.choice(proxypool)
-    try:
-        requests.get('http://www.ip.cn',proxies=proxy)
-    except:
-        chooseproxy()
-    else:
-        proxies = {'http':'http://%s:%s' %(proxy[0],proxy[1])}
-        return proxies
-
+    finally:
+        result.flush()
 
 
 if __name__ == "__main__":
 
-    lock = threading.Lock()
+    lock = Lock()
     result = open('result.txt','a+')
     result.write('关键词\t频道数\t完全包含内容数\n')
     
     
-    text = open(sys.argv[1],'r').readlines()[1030:]
+    text = open(sys.argv[1],'r').readlines()
     while text:
         lines = text[:os.cpu_count()]        #进程数量
         text = text[os.cpu_count():]
-        threads = []
+        ps = []
         for keyword in lines:
             keyword = keyword.strip()
-            t = threading.Thread(target=run,args=(keyword,))
-            threads.append(t)
-            t.start()
+            p = Process(target=run,args=(keyword,))
+            ps.append(p)
+            p.start()
     
     
-        for t in threads:
-            t.join()
+        for p in ps:
+            p.join()
     
         time.sleep(1)
     
