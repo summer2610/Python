a981b81d3377e2914a95cb9747683c2f0f77b629
ginson <kingsionwang@gmail.com>
2017-08-08 08:57:36 +0800

    针对百度最新调整修改着陆页URL获取方式；排名查询工具增加H5支持

--- 关键词百度排名查询工具.py
+++ 关键词百度排名查询工具.py
@@ -13,16 +13,15 @@ python3 bdrank.py 问答关键词.txt 排名数据.txt
 
 import requests,sys,re,os
 from bs4 import BeautifulSoup
-from UA import makeHeaders
-from proxypool import chProxy
+from CONFIG import makeHeaders,getProxy
 from multiprocessing import Process,Lock
 
 def to8to_rank_filter(tag):
     if tag.name != 'div' or not tag.has_attr('class'):
         return False
-    elif 'resultc-container' not in ''.join(tag.attrs['class']):
+    elif 'c-container' not in ''.join(tag.attrs['class']):
         return False
-    elif not tag.find('a','c-showurl',string=re.compile('to8to.com')):
+    elif not tag.find('a','c-showurl',string=re.compile('to8to.com')) and not tag.find('span','c-showurl',string=re.compile('to8to.com')):
         return False
     else:
         return True
@@ -31,22 +30,29 @@ def get_location(landurl):
     r2 = requests.get(landurl,allow_redirects=False)
     return r2.headers['Location']
 
-def get_rank_data(tag,keyword):
-    rank = tag.attrs['id']
-    rankurl = tag.find('a','c-showurl').attrs['href']
-    landurl = get_location(rankurl)
-    return [keyword,rank,landurl]
-
-def run(url,filename,lock):
+def get_rank_data(tag,keyword,env='PC'):
+    if env == 'PC':
+        rank = tag.attrs['id']
+        rankurl = tag.find('a','c-showurl').attrs['href']
+        landurl = get_location(rankurl)
+        return [keyword,rank,landurl]
+    elif env == 'H5':
+        rank = tag.find_parent('div')['order']
+        rankurl = tag.a['href']
+        landurl = get_location(rankurl)
+    else:
+        raise KeyError
+        
+def run(url,filename,lock,env):
     keyword = re.split('[=&]',url)[1]
     try:
-        r = requests.get(url,headers=makeHeaders(),proxies=chProxy(),timeout=10)
+        r = requests.get(url,headers=makeHeaders(),proxies=getProxy(),timeout=10)
     except Exception as e:
         # lock.acquire()
         # with open('failed.log','a+') as log:
         #     log.write('%s\t%s\n' %(keyword,e))
         # lock.release()
-        pass
+        print(e)
     else:
         s = BeautifulSoup(r.text,'lxml')
         to8toRanks = s.findAll(to8to_rank_filter)
@@ -74,6 +80,7 @@ if __name__ == "__main__":
         ps = []
         for searchWord in team:
             url = 'http://www.baidu.com/s?wd=%s&rn=50' %searchWord.strip()
+            #url = 'http://m.baidu.com/s?wd=%s' %searchWord.strip()
             p = Process(target=run,args=(url,sys.argv[2],lock))
             ps.append(p)
             p.start()
--- 百度着陆页URL获取.py
+++ 百度着陆页URL获取.py
@@ -6,14 +6,24 @@
 '''
 
 import requests,sys
+from bs4 import BeautifulSoup
+from CONFIG import getProxy
 
-def get_by_url(bdurl):
-    return requests.head(bdurl,allow_redirects=False).headers['Location']
+def get_location_by_url(bdurl):
+    try:
+        landurl = requests.head(bdurl,proxies=getProxy(),allow_redirects=False).headers['Location']
+    except KeyError:
+        r = requests.get(bdurl)
+        s = BeautifulSoup(r.text,'lxml')
+        landurl = s.find('script').getText().split('(')[-1].split(')')[0].split('"')[1]
+        return landurl
+    else:
+        return landurl
 
 if __name__ == '__main__':
     for bdurl in open(sys.argv[1],'r'):
         try:
-            url = get_by_url(bdurl.strip())
+            url = get_location_by_url(bdurl.strip())
         except:
             with open('失败.txt','a+') as ff:
                 ff.write('%s' %bdurl)
